23c9086a5053f8bb8d8698ddd73e185ddc0c6960
Arjun Das <arjundasback515@gmail.com>
2023-03-10 19:06:32 +0530

    private route loading isu problem solv

--- src/AuthPorvider/AuthProvider.js
+++ src/AuthPorvider/AuthProvider.js
@@ -7,13 +7,17 @@ export const AuthContext = createContext()
 const auth = getAuth(app);
 const AuthProvider = ({children}) => {
     const [user,setUser]=useState(null);
+    const [loading,setLoading]=useState(true)
 
     const SinginProvider = (provider) =>{
+        setLoading(true)
        return signInWithPopup(auth, provider)
     }
     useEffect(()=>{
+        
        const unsubscribe= onAuthStateChanged(auth,(currentUser)=>{
             setUser(currentUser)
+            setLoading(false)
         })
         return ()=>{
             unsubscribe();
@@ -21,17 +25,20 @@ const AuthProvider = ({children}) => {
     },[])
 
     const SingOutAuth =()=>{
+        setLoading(true)
         return signOut(auth)
     }
     const CrateUser =(email,password)=>{
+        setLoading(true)
       return  createUserWithEmailAndPassword(auth,email,password)
     }
 
     const setUserIn =(email, password)=>{
+        setLoading(true)
         return signInWithEmailAndPassword(auth, email, password)
     }
 
-    const authInfo = {user,SinginProvider,SingOutAuth,CrateUser,setUserIn}
+    const authInfo = {user,SinginProvider,SingOutAuth,CrateUser,setUserIn,loading}
     return (
         <AuthContext.Provider value={authInfo}>
             {children}
--- src/Routes/Routes/PrivateRoute.js
+++ src/Routes/Routes/PrivateRoute.js
@@ -1,10 +1,16 @@
 import React, { useContext } from 'react';
 import { AuthContext } from '../../AuthPorvider/AuthProvider';
 import { Navigate, useLocation } from 'react-router-dom';
+import Spinner from 'react-bootstrap/Spinner';
 
 const PrivateRoute = ({children}) => {
-   const {user}=useContext(AuthContext);
+   const {user,loading}=useContext(AuthContext);
    const location=useLocation()
+   if(loading){
+    return <Spinner className='' animation="border" role="status">
+    <span className="visually-hidden">Loading...</span>
+  </Spinner>
+   }
    if(!user){
     return <Navigate to="/login" state={{from:location}} replace></Navigate>
    }
